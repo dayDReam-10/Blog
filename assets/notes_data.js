624cda0c9ab9bc5c8726b760bf200142931c3570
daydream <3380971468@example.com>
2026-04-04 14:41:20 +0800

    test_blog_update

--- assets/notes_data.js
+++ assets/notes_data.js
@@ -13,18 +13,18 @@
         "title":  "全面模块化回归",
         "datetime":  "2024.03.31 / 15:45",
         "status":  "System Update",
-        "content":  "TEST",
+        "content":  "# happy_new_year Writeup\u003cbr\u003e\u003cbr\u003e（在此感谢某位re手法哥的指导）\u003cbr\u003e\u003cbr\u003e最近在学习动态调试，偶然看自己的题解发现这一题可以用x32dbg试试手动脱壳练练动调\u003cbr\u003e\u003cbr\u003e来！说来就来！\u003cbr\u003e\u003cbr\u003e先说一下upx压缩壳原理，其实是用了pushad把寄存器先保存到栈顶，然后执行加密压缩什么的操作\u003cbr\u003e\u003cbr\u003e由于程序最后一定能执行，所以我们知道这个upx一定存在还原点，我们的任务就是找出还原点（popad）\u003cbr\u003e\u003cbr\u003e```text\u003cbr\u003e【内存地址】       【栈内存里的内容】\u003cbr\u003e0019FF70          [ 原始程序的数据 A ]  \u003c-- 初始 ESP (原来的数据)\u003cbr\u003e------------------------------------",
         "date":  "2024-03-31",
         "idDate":  "20240331"
     },
     {
-        "color":  "#d4f871",
-        "title":  "开场动画逻辑检查",
-        "datetime":  "2024.03.29 / 22:10",
-        "status":  "Hotfix",
-        "content":  "$TEST",
-        "date":  "2024-03-29",
-        "idDate":  "20240329"
+        "color":  "#4ade80",
+        "title":  "全面模块化回归",
+        "datetime":  "2024.03.31 / 15:45",
+        "status":  "System Update",
+        "content":  "TEST",
+        "date":  "2024-03-31",
+        "idDate":  "20240331"
     },
     {
         "color":  "#f87171",
